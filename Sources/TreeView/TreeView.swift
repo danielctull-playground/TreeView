8efc237d4e3636fe1d56e5288fc4f784f1fb8d96
Daniel Tull <dt@danieltull.co.uk>
2020-03-28 12:51:07 +0000

    Extract ItemsView to its own internal view

--- Sources/TreeView/TreeView.swift
+++ Sources/TreeView/TreeView.swift
@@ -1,12 +1,10 @@
 
 import SwiftUI
 
-public struct TreeView<Value, Content>: View
-    where
-Content: View {
+public struct TreeView<Value, Content: View>: View {
 
-    let tree: Tree<Value>
-    let content: (Value) -> Content
+    fileprivate let tree: Tree<Value>
+    fileprivate let content: (Value) -> Content
 
     public init(tree: Tree<Value>,
                 content: @escaping (Value) -> Content) {
@@ -15,11 +13,21 @@ Content: View {
     }
 
     public var body: some View {
+        ItemsView(tree: tree, content: content)
+    }
+}
+
+fileprivate struct ItemsView<Value, Content: View>: View {
+
+    let tree: Tree<Value>
+    let content: (Value) -> Content
+
+    var body: some View {
         VStack {
             content(tree.value)
             HStack(alignment: .top) {
                 ForEach(tree.children.indices) { index in
-                    TreeView(tree: self.tree.children[index], content: self.content)
+                    ItemsView(tree: self.tree.children[index], content: self.content)
                 }
             }
         }
